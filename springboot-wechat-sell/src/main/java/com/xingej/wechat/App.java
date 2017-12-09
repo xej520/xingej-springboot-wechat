ea897d1a23b58fdf81542f61ad220410d19f9f0b
邢尔军 <xingej@163.com>
2017-12-09 09:24:02 +0800

    创建springboot 微信点餐系统  项目

---
+++ springboot-wechat-sell/src/main/java/com/xingej/wechat/App.java
@@ -0,0 +1,13 @@
+package com.xingej.wechat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
