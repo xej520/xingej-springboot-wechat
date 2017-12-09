194e1306b0cc8961d9e3dcc147d8096a38dbb3d8
邢尔军 <xingej@163.com>
2017-12-09 14:52:46 +0800

    添加注解@DynamicUpdate ，配合数据库自动更新时间戳的功能

--- springboot-wechat-sell/src/main/java/com/xingej/wechat/dataobject/ProductCategory.java
+++ springboot-wechat-sell/src/main/java/com/xingej/wechat/dataobject/ProductCategory.java
@@ -1,8 +1,12 @@
 package com.xingej.wechat.dataobject;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.DynamicUpdate;
 
 /**
  * 类目表
@@ -11,6 +15,8 @@ import javax.persistence.Id;
  */
 
 @Entity // 将数据库映射成java 对象
+@DynamicUpdate // 这个注解的作用，就是，更新时间自动变化
+// 我现在的本地环境是失败的，由于，数据库的版本比较低，5.5的，好像得5.6以上，数据库才能支持自动更新时间戳
 public class ProductCategory {
 
     // 类目ID
@@ -23,6 +29,10 @@ public class ProductCategory {
 
     // 类目编号
     private Integer categoryType;
+
+    private Date createTime;
+
+    private Date updateTime;
 
     public Integer getCategoryId() {
         return categoryId;
@@ -46,6 +56,28 @@ public class ProductCategory {
 
     public void setCategoryType(Integer categoryType) {
         this.categoryType = categoryType;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory [categoryId=" + categoryId + ", categoryName=" + categoryName + ", categoryType="
+                + categoryType + ", createTime=" + createTime + ", updateTime=" + updateTime + "]";
     }
 
 }
