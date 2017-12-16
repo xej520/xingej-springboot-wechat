ee32ba4dd81fe5b0036ce2644e1ff7cd2d16ed2c
邢尔军 <xingej@163.com>
2017-12-16 10:25:27 +0800

    订单详情表完成

---
+++ springboot-wechat-sell/src/main/java/com/xingej/wechat/dataobject/OrderDetail.java
@@ -0,0 +1,99 @@
+package com.xingej.wechat.dataobject;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * 订单详情表
+ * 
+ * @author erjun 2017年12月16日 上午10:23:56
+ */
+// 数据表，映射成java实体类
+@Entity
+public class OrderDetail {
+
+    @Id
+    @GenericGenerator(name = "uuidGenerator", strategy = "uuid")
+    @GeneratedValue(generator = "uuidGenerator")
+    private String detailId;
+
+    /** 订单id. */
+    private String orderId;
+
+    /** 商品id. */
+    private String productId;
+
+    /** 商品名称. */
+    private String productName;
+
+    /** 商品单价. */
+    private BigDecimal productPrice;
+
+    /** 商品数量. */
+    private Integer productQuantity;
+
+    /** 商品小图. */
+    private String productIcon;
+
+    public String getDetailId() {
+        return detailId;
+    }
+
+    public void setDetailId(String detailId) {
+        this.detailId = detailId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(BigDecimal productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Integer getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(Integer productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public String getProductIcon() {
+        return productIcon;
+    }
+
+    public void setProductIcon(String productIcon) {
+        this.productIcon = productIcon;
+    }
+
+}
